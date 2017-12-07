478403d4072204d41a15cb005e8449a59ec2def7
Steven <schen36@stuy.edu>
2017-12-06 23:27:28 -0500

    done?

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -15,14 +15,48 @@ public class Sorts {
 	return true;
     }
 
+    public static void swap(int[] ary, int a, int b){ 
+	int temp = ary[a];
+	ary[a] = ary[b];
+	ary[b] = temp;
+    }
+
     public static void selectionSort(int[] data) {
-	
-	
-	
-	
+        if (!isSorted(data)) {
+	    for (int i = 0; i < data.length; i++) {
+		int smallestIndex = i;
+		for (int x = i; x < data.length; x ++) {
+		    if (data[x] < data[smallestIndex]) {
+			smallestIndex = x;
+		    }
+		}
+		swap(data, i, smallestIndex);
+	    }
+	}
+    }
+    
+    public static void insertionSort(int[] data) {
+	while (!isSorted(data)) {
+	    for (int i = 1; i < data.length; i++) {
+		int key = data[i];
+		int x = i - 1;
+		while (x >= 0 && data[x] > key) {
+		    data[x + 1] = data[x];
+		    x = x - 1;
+		}
+		data[x + 1] = key;
+	    }
+	}
     }
 
     public static void main(String[] args) {
-	
+	int[] bleh = new int[5];
+	for (int i = 0; i < bleh.length; i ++) {
+	    bleh[i] = (int)(Math.random() * 100);
+	}
+
+	System.out.println(Arrays.toString(bleh));
+        insertionSort(bleh);
+	System.out.println(Arrays.toString(bleh));
     }
 }
